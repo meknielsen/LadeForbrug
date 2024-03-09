4b6b8ae88aee531bb226eb3b881089a31225f7da
mek.nielsen@gmail.com <mek.nielsen@gmail.com>
2024-03-09 17:19:15 +0100

    .

--- svelte.config.js
+++ svelte.config.js
@@ -16,6 +16,11 @@ const config = {
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
+    alias: {
+      $components: "src/components",
+      $lib: "src/components",
+      $routes: "src/routes",
+    },
   },
 
   preprocess: [vitePreprocess({})],
